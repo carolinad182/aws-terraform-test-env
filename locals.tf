59e1276c32977ac5dba8790d4bb61b0c08a65664
carolinad182 <carolina_diazp18@hotmail.com>
2022-08-15 10:34:56 -0400

    updating architecture, adding new provider0

--- locals.tf
+++ locals.tf
@@ -1,7 +1,16 @@
+resource "random_integer" "rand" {
+  min = 10000
+  max = 99000
+}
+
 locals {
   common_tags = {
     company      = var.company
     project      = "${var.company}-${var.project}"
     billing_code = var.billing_code
   }
+
+  s3_bucket_name = "globo-web-app-${random_integer.rand.result}"
 }
+
+
